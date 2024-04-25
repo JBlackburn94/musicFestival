98106227bdb0e06256f80fd34ce720b7c945aa03
Jason Blackburn <113805105+JBlackburn94@users.noreply.github.com>
2024-04-25 20:58:39 +0100

    marquee added

--- music-festival/src/components/banner/Banner.jsx
+++ music-festival/src/components/banner/Banner.jsx
@@ -1,7 +1,28 @@
+import Marquee from "react-fast-marquee";
+import "./banner.css";
+
 export default function Banner() {
   return (
-    <div>
-      <p>banner</p>
+    <div className="mt-16 md:mt-0 bg-bright-turquoise-400 text-bright-turquoise-900 hover:bg-bright-turquoise-200">
+      <Marquee pauseOnHover>
+        <a href="#" className="flex p-4">
+          <p className="mx-4 tracking-wider">
+            buy MFE 2023 leftover merch here!
+          </p>
+          <p className="mx-4 tracking-wider">
+            buy MFE 2023 leftover merch here!
+          </p>
+          <p className="mx-4 tracking-wider">
+            buy MFE 2023 leftover merch here!
+          </p>
+          <p className="mx-4 tracking-wider">
+            buy MFE 2023 leftover merch here!
+          </p>
+          <p className="mx-4 tracking-wider">
+            buy MFE 2023 leftover merch here!
+          </p>
+        </a>
+      </Marquee>
     </div>
   );
 }
--- music-festival/src/components/hero/Hero.jsx
+++ music-festival/src/components/hero/Hero.jsx
@@ -13,13 +13,13 @@ export default function Hero() {
         </h2>
       </div>
       <div className="flex items-center justify-center w-full mt-10 lg:mt-0 lg:w-1/2">
-        <video muted autoPlay loop className="w-1/4 mr-4">
+        <video muted autoPlay loop className="w-1/2 mr-4 md:w-1/4">
           <source src={heroVideo} type="video/mp4" />
           Your browser does not support the video tag.
         </video>
 
         <h2
-          className="relative w-1/4 text-5xl md:text-6xl right-10 text-bright-turquoise-100 lg:right-16"
+          className="relative w-1/4 text-5xl md:text-6xl right-16 md:right-10 text-bright-turquoise-100 lg:right-16"
           id="sub-text"
         >
           Get <br /> <span className="text-bright-turquoise-400">VIP</span>{" "}
