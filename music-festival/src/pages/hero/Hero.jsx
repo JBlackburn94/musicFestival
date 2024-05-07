75f8f337ca46a7c6167b3a58a342b37d7f9ba598
Jason Blackburn <113805105+JBlackburn94@users.noreply.github.com>
2024-05-07 19:46:37 +0100

    added sponsor section and mobile responsiveness

--- music-festival/src/pages/hero/Hero.jsx
+++ music-festival/src/pages/hero/Hero.jsx
@@ -2,7 +2,13 @@ import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { useRef } from "react";
 import heroVid from "../../assets/hero.mp4";
-import sponsors from "../../assets/Group 6.webp";
+import bbc from "../../assets/bbc.webp";
+import heineken from "../../assets/heineken.webp";
+import twitch from "../../assets/twitch.webp";
+import spotify from "../../assets/spotify.webp";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(useGSAP, ScrollTrigger);
 
 export default function Hero() {
   const containerLeft = useRef(null);
@@ -31,28 +37,44 @@ export default function Hero() {
       opacity: 0,
       delay: 1.8,
     });
+    gsap.to(containerLeft.current, {
+      opacity: 0,
+      ScrollTrigger: {
+        trigger: containerLeft.current,
+        start: "top center",
+        end: "bottom center",
+        markers: true,
+      },
+    });
   });
 
   return (
-    <section className="flex flex-col">
-      <div className="flex flex-row items-start justify-between">
+    <section className="flex flex-col overflow-x-hidden">
+      <div className="flex flex-col items-center justify-center lg:items-start lg:justify-between lg:flex-row">
         <div
           ref={containerLeft}
-          className="h-[550px] w-1/2 bg-[#75D8FF] rounded-br-full relative flex items-center justify-center flex-col"
+          className="h-[550px] w-full lg:w-1/2 bg-[#75D8FF] rounded-br-full relative flex items-center justify-center flex-col"
         >
           <span ref={leftContent} className="overflow-hidden">
-            <h1 className="sub-text text-[#04314D] text-8xl">MFE</h1>
-            <h2 className="font-black text-8xl">Festival</h2>
+            <h1 className="sub-text text-[#04314D] text-5xl lg:text-8xl">
+              MFE
+            </h1>
+            <h2 className="text-5xl font-black lg:text-8xl">Festival</h2>
             <h2 className="text-4xl font-bold text-center text-[#04314D]">
               2024
             </h2>
           </span>
         </div>
         <div
-          className="flex flex-col items-center justify-center w-1/2 mt-4"
+          className="flex flex-col items-center justify-center w-full mt-4 lg:w-1/2"
           ref={containerRight}
         >
-          <video className="w-[310px] rounded-lg" autoPlay loop muted>
+          <video
+            className="w-[300px] lg:w-[310px] rounded-xl"
+            autoPlay
+            loop
+            muted
+          >
             <source src={heroVid} />
           </video>
           <button
@@ -63,12 +85,30 @@ export default function Hero() {
           </button>
         </div>
       </div>
-      <div className="mt-[86px] mx-[225px]">
+      <div className="mt-[86px] flex flex-col items-center justify-center">
         <h3 className="text-4xl font-black uppercase">Sponsors</h3>
-        <img
-          src={sponsors}
-          alt="An image of 4 sponsors, Heineken, BBC, Spotify and Twitch"
-        />
+        <div className="flex flex-col items-center justify-center lg:flex-row">
+          <img
+            src={bbc}
+            alt="An image of the BBC logo"
+            className="min-w-[100px] max-w-[150px] m-4 h-auto"
+          />
+          <img
+            src={heineken}
+            alt="An image of the Heineken logo"
+            className="min-w-[100px] max-w-[200px] m-4 h-auto"
+          />
+          <img
+            src={twitch}
+            alt="An image of the Twitch logo"
+            className="min-w-[100px] max-w-[200px] m-4 h-auto"
+          />
+          <img
+            src={spotify}
+            alt="An image of the Spotify logo"
+            className="min-w-[100px] max-w-[130px] m-4 h-auto"
+          />
+        </div>
       </div>
     </section>
   );
