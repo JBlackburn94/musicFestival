bb4acfe08a9ef53a2916b5c2d5f8170d516d03ef
Jason Blackburn <113805105+JBlackburn94@users.noreply.github.com>
2024-05-09 10:49:59 +0100

    content added to ticket section

--- music-festival/src/pages/tickets/Ticket.jsx
+++ music-festival/src/pages/tickets/Ticket.jsx
@@ -1,10 +1,16 @@
 export default function Tickets() {
   return (
-    <section className="bg-[#B6E8FF] h-auto overflow-hidden">
+    <section className="bg-[#B6E8FF] h-auto overflow-hidden mt-10">
       <div className="absolute z-10 flex flex-col items-center justify-center w-full h-screen lg:flex-row">
-        <div className="w-[384px] h-[505px] my-5 lg:my-0 bg-[#04314D] rounded-lg mx-0 lg:mx-4"></div>
-        <div className="w-[384px] h-[505px] my-5 lg:my-0 lg:h-[548px] bg-[#04314D] rounded-lg mx-0 lg:mx-4"></div>
-        <div className="w-[384px] h-[505px] my-5 lg:my-0 bg-[#04314D] rounded-lg mx-0 lg:mx-4"></div>
+        <div className="w-[384px] h-[505px] my-5 lg:my-0 bg-[#04314D] rounded-lg mx-0 lg:mx-4 flex flex-col items-center justify-center font-bold uppercase text-xl md:text-2xl">
+          Weekend
+        </div>
+        <div className="w-[384px] h-[505px] my-5 lg:my-0 lg:h-[548px] bg-[#04314D] rounded-lg mx-0 lg:mx-4 flex flex-col items-center justify-center font-bold uppercase text-xl md:text-2xl">
+          VIP
+        </div>
+        <div className="w-[384px] h-[505px] my-5 lg:my-0 bg-[#04314D] rounded-lg mx-0 lg:mx-4 flex flex-col items-center justify-center font-bold uppercase text-xl md:text-2xl">
+          Day
+        </div>
       </div>
       <div className="bg-[#04314D] h-screen w-1/2 rounded-r-full opacity-30 relative z-0"></div>
     </section>
