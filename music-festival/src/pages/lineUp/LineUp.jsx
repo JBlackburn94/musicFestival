f1c8d60de69eb676e08a7dd54a948f5c10c9f43f
Jason Blackburn <113805105+JBlackburn94@users.noreply.github.com>
2024-05-09 14:07:13 +0100

    spacing on lineup section altered

--- music-festival/src/pages/lineUp/LineUp.jsx
+++ music-festival/src/pages/lineUp/LineUp.jsx
@@ -8,6 +8,7 @@ gsap.registerPlugin(useGSAP);
 export default function LineUp() {
   const containerLeft = useRef(null);
   const heading = useRef(null);
+  const containerLeftTwo = useRef(null);
 
   useGSAP(() => {
     gsap.from(containerLeft.current, {
@@ -19,7 +20,16 @@ export default function LineUp() {
         start: "50% 95%",
         end: "70% 80%",
         scrub: 1,
-        marekers: true,
+      },
+    });
+    gsap.from(containerLeftTwo.current, {
+      opacity: 0,
+      x: "-100%",
+      scrollTrigger: {
+        trigger: containerLeft.current,
+        start: "50% 70%",
+        end: "35% 45%",
+        scrub: 1,
       },
     });
     gsap.from(heading.current, {
@@ -40,10 +50,14 @@ export default function LineUp() {
         className="h-full w-screen relative bg-[#75D8FF]"
       ></div>
       <div
+        ref={containerLeftTwo}
+        className="h-full w-3/4 absolute bg-[#04314D] rounded-r-full"
+      ></div>
+      <div
         ref={heading}
-        className="absolute flex flex-col items-center justify-center w-full mt-[100px]"
+        className="absolute flex flex-col items-center justify-center w-full h-screen"
       >
-        <h2 className="text-8xl sub-text">Line Up</h2>
+        <h2 className="text-6xl uppercase md:text-8xl sub-text">Line Up</h2>
         <img
           src={lineUp}
           alt="A fictional line up poster for MFE"
