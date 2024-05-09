ccbe63b343252569cd058eef2e6ab03b0685ec46
Jason Blackburn <113805105+JBlackburn94@users.noreply.github.com>
2024-05-09 12:49:43 +0100

    lineup poster added

--- music-festival/src/pages/lineUp/LineUp.jsx
+++ music-festival/src/pages/lineUp/LineUp.jsx
@@ -1,25 +1,15 @@
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { useRef } from "react";
+import lineUp from "../../assets/GeneratedFestivalPoster.webp";
 
 gsap.registerPlugin(useGSAP);
 
 export default function LineUp() {
-  const containerRight = useRef(null);
   const containerLeft = useRef(null);
   const heading = useRef(null);
 
   useGSAP(() => {
-    gsap.from(containerRight.current, {
-      opacity: 0,
-      x: "100%",
-      scrollTrigger: {
-        trigger: containerLeft.current,
-        start: "60% 90%",
-        end: "60% 60%",
-        scrub: 1,
-      },
-    });
     gsap.from(containerLeft.current, {
       opacity: 0,
       x: "-100%",
@@ -32,7 +22,7 @@ export default function LineUp() {
     });
     gsap.from(heading.current, {
       opacity: 0,
-      y: "-100%",
+      x: "100%",
       scrollTrigger: {
         trigger: containerLeft.current,
         start: "60% 90%",
@@ -46,18 +36,19 @@ export default function LineUp() {
     <section className="flex flex-col h-screen overflow-x-hidden">
       <div
         ref={containerLeft}
-        className="h-1/2 w-1/2 rounded-br-full relative bg-[#75D8FF]"
+        className="h-full w-1/2 rounded-br-full relative bg-[#75D8FF]"
       ></div>
       <div
         ref={heading}
-        className="absolute flex items-center justify-center w-full mt-[100px]"
+        className="absolute flex flex-col items-center justify-center w-full mt-[100px]"
       >
         <h2 className="text-8xl sub-text">Line Up</h2>
+        <img
+          src={lineUp}
+          alt="A fictional line up poster for MFE"
+          className="min-w-[400px] w-1/4 mt-10 lg:w-1/2"
+        />
       </div>
-      <div
-        ref={containerRight}
-        className="h-1/2 w-1/2 bottom-0 sticky left-[100%] rounded-tl-full bg-[#75D8FF]"
-      ></div>
     </section>
   );
 }
